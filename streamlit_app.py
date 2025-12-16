be2c5b84e071de02033ddf544d3b3c46a26035e0
tringwelski <t.ringwelski@simplicity.ag>
2025-12-16 11:08:13 +0000

    move the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -57,8 +60,3 @@ if ingredients_list:
         if my_insert_stmt:
             session.sql(my_insert_stmt).collect()
             st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
-
-# New Section to display smoothiefroot nutrition information
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
